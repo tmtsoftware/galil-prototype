95bb3b0159df387b8467c6332dd6ba60674c81f2
abrighton <78px3WT5s>
2017-09-30 21:33:19 +0200

    Testing

--- galil-repl/src/main/scala/csw/proto/galil/simulatorRepl/QrCmd.scala
+++ galil-repl/src/main/scala/csw/proto/galil/simulatorRepl/QrCmd.scala
@@ -1,66 +1,42 @@
 package csw.proto.galil.simulatorRepl
 
 import akka.util.ByteString
-import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
 object QrCmd {
 
-  private def getBit(num: Byte, i: Int): Boolean = (num & (1 << i)) != 0
-//  private def getBit(num: Byte, i: Int): Boolean = ((num >> i) & 0x01) == 1
+  private def getBit(num: Byte, i: Int): Boolean =
+    (num & (1 << i)) != 0
 
-  private def getBlock(num: Byte, i: Int, s: String): String = {
+  private def getBlock(num: Byte, i: Int, s: String): String =
     if (getBit(num, i)) s else ""
-  }
-
-  private def getUnsignedShort(b1: Byte, b2: Byte): Int = {
-    val bb = ByteBuffer.allocate(2).put(b1).put(b2).order(ByteOrder.LITTLE_ENDIAN)
-    bb.flip()
-    bb.getShort() & 0xFFFF
-  }
 
   /**
     * Formats the binary result of a Galil QR command.
     * See DMC-500x0 User Manual: Chapter 4 Software Tools and Communication, p 58.
     */
   def format(bs: ByteString): String = {
-    println(s"XXX bs size = ${bs.size}")
     if (bs.size < 4) {
       "error: missing QR header"
     } else {
-      // 4 byte header
-      val b0 = bs(0)
-      val b1 = bs(1)
-      val testBit = getBit(b0, 7)
-      println(s"XXX test bit set: $testBit")
-      val blocksPresent = List(
-        getBlock(b0, 0, "S"),
-        getBlock(b0, 1, "T"),
-        getBlock(b0, 2, "I"),
-        getBlock(b1, 0, "A"),
-        getBlock(b1, 1, "B"),
-        getBlock(b1, 2, "C"),
-        getBlock(b1, 3, "D"),
-        getBlock(b1, 4, "E"),
-        getBlock(b1, 5, "F"),
-        getBlock(b1, 6, "G"),
-        getBlock(b1, 7, "H")).mkString(" ")
-
-      val recordSize = getUnsignedShort(bs(2),bs(3))
-
-      // XXX
       val buffer = bs.toByteBuffer.order(ByteOrder.LITTLE_ENDIAN)
+      // 4 byte header
+      val byte0 = buffer.get
+      val byte1 = buffer.get
+      val blocksPresent = List(
+        getBlock(byte0, 0, "S"),
+        getBlock(byte0, 1, "T"),
+        getBlock(byte0, 2, "I"),
+        getBlock(byte1, 0, "A"),
+        getBlock(byte1, 1, "B"),
+        getBlock(byte1, 2, "C"),
+        getBlock(byte1, 3, "D"),
+        getBlock(byte1, 4, "E"),
+        getBlock(byte1, 5, "F"),
+        getBlock(byte1, 6, "G"),
+        getBlock(byte1, 7, "H")).mkString(", ")
 
-      val byte0: Byte = buffer.get
-      if (byte0 != b0) println(s"XXX byte0: $byte0 != $b0")
-      if (((byte0 >> 7) & 0x01) != 1) { // The MSB of the first byte must be always 1.
-        println("The MSB of the first byte in the Data Record header is not one.")
-      }
-      val byte1: Byte = buffer.get
-      if (byte1 != b1) println(s"XXX byte1: $byte1 != $b1")
-      val len = buffer.getShort() & 0xFFFF
-      println(s"XXX CHECK: len = $len")
-      // XXX
+      val recordSize = buffer.getShort() & 0xFFFF
 
       s"""
          |Blocks present:   $blocksPresent
