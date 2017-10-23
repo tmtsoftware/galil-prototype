9b392718279c85a662aa523dbb33a888849c0cab
weiss <weiss.personal@gmail.com>
2017-10-23 16:12:01 -0700

    Fixed logger, changed to onSubmit and onOneway per csw-prod changes

--- galil-assembly/src/main/scala/csw/proto/galil/assembly/GalilAssembly.scala
+++ galil-assembly/src/main/scala/csw/proto/galil/assembly/GalilAssembly.scala
@@ -1,22 +1,20 @@
 package csw.proto.galil.assembly
 
-import java.io.FileWriter
-import java.nio.file.{Files, Path}
-
 import akka.typed.ActorRef
 import akka.typed.scaladsl.ActorContext
 import com.typesafe.config.ConfigFactory
 import csw.apps.containercmd.ContainerCmd
 import csw.framework.scaladsl.{ComponentBehaviorFactory, ComponentHandlers}
-import csw.messages._
 import csw.messages.PubSub.PublisherMessage
 import csw.messages.RunningMessage.DomainMessage
+import csw.messages._
+import csw.messages.ccs.commands.ControlCommand
 import csw.messages.ccs.{Validation, ValidationIssue, Validations}
 import csw.messages.framework.ComponentInfo
 import csw.messages.location.TrackingEvent
 import csw.messages.params.states.CurrentState
 import csw.services.location.scaladsl.LocationService
-import csw.services.logging.scaladsl.ComponentLogger
+import csw.services.logging.scaladsl.CommonComponentLogger
 
 import scala.async.Async._
 import scala.concurrent.{ExecutionContextExecutor, Future}
@@ -36,28 +34,29 @@ private class GalilAssemblyBehaviorFactory extends ComponentBehaviorFactory[Gali
     new GalilAssemblyHandlers(ctx, componentInfo, pubSubRef, locationService)
 }
 
+
+object GalilAssemblyLogger extends CommonComponentLogger("GalilAssembly")
+
 private class GalilAssemblyHandlers(ctx: ActorContext[ComponentMessage],
                                     componentInfo: ComponentInfo,
                                     pubSubRef: ActorRef[PubSub.PublisherMessage[CurrentState]],
                                     locationService: LocationService)
   extends ComponentHandlers[GalilAssemblyDomainMessage](ctx, componentInfo, pubSubRef, locationService)
-    with ComponentLogger.Simple {
+    with GalilAssemblyLogger.Simple {
 
   implicit val ec: ExecutionContextExecutor = ctx.executionContext
-
-  override def componentName(): String = "GalilAssembly"
 
   override def initialize(): Future[Unit] = async {
     log.debug("Initialize called")
   }
 
-  override def onSetup(commandMessage: CommandMessage): Validation = {
-    log.debug(s"onSetup called: $commandMessage")
+  override def onSubmit(controlCommand: ControlCommand, replyTo: ActorRef[CommandResponse]): Validation = {
+    log.debug(s"onSetup called: $controlCommand")
     Validations.Valid
   }
 
-  override def onObserve(commandMessage: CommandMessage): Validation =  {
-    log.debug(s"onObserve called: $commandMessage")
+  override def onOneway(controlCommand: ControlCommand): Validation = {
+    log.debug(s"onObserve called: $controlCommand")
     Validations.Invalid(ValidationIssue.UnsupportedCommandIssue("Observe  not supported"))
   }
 
