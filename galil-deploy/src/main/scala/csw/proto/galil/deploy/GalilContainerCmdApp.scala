72e6b6c9462a1640e021ebee399908b9caf7910c
weiss <weiss.personal@gmail.com>
2018-02-26 21:57:13 -0800

    Added cmd name

--- galil-deploy/src/main/scala/csw/proto/galil/deploy/GalilContainerCmdApp.scala
+++ galil-deploy/src/main/scala/csw/proto/galil/deploy/GalilContainerCmdApp.scala
@@ -4,5 +4,5 @@ import com.typesafe.config.ConfigFactory
 import csw.apps.containercmd.ContainerCmd
 
 object GalilContainerCmdApp extends App {
-  ContainerCmd.start("", args)
+  ContainerCmd.start("GalilContainerCmd", args)
 }
