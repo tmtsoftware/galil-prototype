72292ce4c3f5ffbd37da386c7855fd0610c69d3e
weiss <weiss.personal@gmail.com>
2018-02-28 10:55:32 -0800

    Created CSW version to keep separate from prototype version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,7 @@ import sbt._
 object Dependencies {
 
   val Version = "0.1-SNAPSHOT"
+  val CswVersion = "0.1-SNAPSHOT"
   val ScalaVersion = "2.12.4"
   val PlayVersion = "2.6.5"
 
@@ -11,7 +12,7 @@ object Dependencies {
   val `junit-interface` = "com.novocode" % "junit-interface" % "0.11"
   val playJson = "com.typesafe.play" %% "play-json" % PlayVersion
 
-  val `csw-framework`  = "org.tmt" %% "csw-framework"  % Version
+  val `csw-framework`  = "org.tmt" %% "csw-framework"  % CswVersion
   val `galil-assembly-dep` = "org.tmt" %% "galil-assembly" % Version
   val `galil-hcd-dep`      = "org.tmt" %% "galil-hcd"      % Version
 }
